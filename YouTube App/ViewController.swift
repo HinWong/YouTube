9d8497d6108b0dda1f6dd30777660429bbfa1d0f
Hin Wong <hinsolo@Hins-MacBook-Pro.local>
2020-07-03 16:05:02 -0400

    Initial project creation

---
+++ YouTube App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube App
+//
+//  Created by Hin Wong on 7/3/20.
+//  Copyright © 2020 Hin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
